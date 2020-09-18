d308ba42201b28c5b249d221e97285921c4040c2
Oliver <wang59651@163.com>
2020-09-18 15:31:04 +0800

    prettier+eslint配置完成

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,36 @@
-/*
- * @Author: your name
- * @Date: 2020-03-26 08:59:56
- * @LastEditTime: 2020-09-18 14:04:16
- * @LastEditors: wangchaoxu
- * @Description: In User Settings Edit
- * @FilePath: \weidun\.eslintrc.js
- */
 module.exports = {
-  root: true,
   env: {
     browser: true,
     node: true,
   },
-  parser: "@typescript-eslint/parser",
-  extends: ["plugin:@typescript-eslint/recommended", "react-app", "plugin:prettier/recommended"],
+  parser: "@typescript-eslint/parser", // 指定ESLint解析器
+  extends: [
+    "plugin:react/recommended", // 使用来自 @eslint-plugin-react 的推荐规则
+    "plugin:@typescript-eslint/recommended", // 使用来自@typescript-eslint/eslint-plugin的推荐规则
+    "prettier/@typescript-eslint", // 使用 ESLint -config-prettier 禁用来自@typescript-eslint/ ESLint 与 prettier 冲突的 ESLint 规则
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 2018, // 允许解析最新的 ECMAScript 特性
+    sourceType: "module", // 允许使用 import
+    ecmaFeatures: {
+      jsx: true, // 允许对JSX进行解析
+    },
+  },
   rules: {
+    // 自定义规则
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     endOfLine: 0,
     "no-unused-vars": 0,
     "no-useless-escape": 0, //忽略转义字符验证
   },
-  plugins: ["@typescript-eslint", "react"],
+  settings: {
+    react: {
+      version: "detect", // 告诉 eslint-plugin-react 自动检测 React 的版本
+    },
+  },
   globals: {
     AMap: true,
-    AMapUI: true,
-    _: true,
-    $: true,
-    Vue: true,
-    cyberplayer: true,
   },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import './App.css';
-import 'cesium/Widgets/widgets.css';
-import { Viewer } from 'cesium';
+import React from "react";
+import "./App.css";
+import "cesium/Widgets/widgets.css";
+import { Viewer } from "cesium";
 class Test extends React.Component {
-	constructor(props: Readonly<{}>) {
-		super(props);
-		this.state = { date: new Date() };
-	}
-	componentDidMount() {
-		console.log('执行一');
-		new Viewer('cesiumContainer');
-	}
+  constructor(props: Readonly<{}>) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+  componentDidMount() {
+    console.log("执行一");
+    new Viewer("cesiumContainer");
+  }
 
-	componentWillUnmount() {
-		console.log('执行二');
-	}
-	render() {
-		return <div id="cesiumContainer"></div>;
-	}
+  componentWillUnmount() {
+    console.log("执行二");
+  }
+  render() {
+    return <div id="cesiumContainer"></div>;
+  }
 }
 
 export default Test;
